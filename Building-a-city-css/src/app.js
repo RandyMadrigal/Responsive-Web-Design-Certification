d3df115f0c41ad5bcc5ea1b5a3825ea89d07344b
Randy Madrigal <randymadrigalx12@gmail.com>
2024-02-22 21:08:41 -0400

    Cambiar color de interfaz entre dia y noche dependiendo de la hora local

---
+++ Building-a-city-css/src/app.js
@@ -0,0 +1,54 @@
+/*
+Si la hora contiene PM cambiar a modo noche,
+en caso contrario modo dia.
+*/
+
+const date = new Date();
+let time = date.toLocaleTimeString();
+
+//propiedades :root
+let properties = {
+  "--building-color1": "#000",
+  "--building-color2": "#000",
+  "--building-color3": "#000",
+  "--building-color4": "#000",
+  "--window-color1": "#777",
+  "--window-color2": "#777",
+  "--window-color3": "#777",
+  "--window-color4": "#777",
+};
+
+//propiedades .sky
+let sky = document.querySelector(".sky");
+
+//aplicar propiedades
+
+function setSkyStyle(prop) {
+  prop.style.setProperty(
+    "background",
+    "radial-gradient(closest-corner circle at 15% 15%,#ccc, #ccc 20%, #445 21%, #223 100%"
+  );
+}
+
+function setRootStyle(property) {
+  //aplicar propiedades :root
+  for (const prop in property) {
+    if (property.hasOwnProperty(prop)) {
+      document.documentElement.style.setProperty(prop, property[prop]);
+    }
+  }
+}
+
+function getTime() {
+  if (time.includes("PM")) {
+    setSkyStyle(sky);
+    setRootStyle(properties);
+  }
+  console.log(time);
+}
+
+getTime();
+
+setInterval(() => {
+  getTime();
+}, 1800000);
